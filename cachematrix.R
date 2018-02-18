65d74e6a114df3583bf7ce2096882140bde0a4d5
rjreeves <robert@reeves.net>
2018-02-18 15:53:21 +1100

    Added code for matrix transpose

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## If the contents of a matrix are not changing, it makes sense to cache the value of the transpose
+## so that when needed again, it can be looked up in the cache rather than recomputed.
 
-## Write a short comment describing this function
+## This function creates a special matrix with functions to set/get a matrix and set/get the transposed-matrix   
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  trn <- NULL
+  set <- function(y) {
+    x <<- y
+    trn <<- NULL
+  }
+  get <- function() x
+  settrn <- function(tranpose) trn <<- tranpose
+  gettrn <- function() trn
+  list(set = set, get = get,
+       settrn = settrn,
+       gettrn = gettrn)
 }
 
 
-## Write a short comment describing this function
+## This function "calculates" transpose of a matrix and cache the result. 
+## If already cached simply provide the cached result, avoidng a re-calc.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  trn<- x$gettrn()
+  if (!is.null(trn)) {
+      message("From the cache")     # Advise the cahce provided answer
+      return(trn)
+  }
+  data<-x$get()
+  trn<- t(data,...)
+  x$settrn(trn)
+  trn
 }
